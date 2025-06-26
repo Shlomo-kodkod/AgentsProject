237d3d9cf7aef2ecf0b94a31fae8a9c5173e69cf
kodcode <quarts.91.elders@icloud.com>
2025-06-26 14:39:15 +0300

    add get agent

--- dal/agent_dal.py
+++ dal/agent_dal.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from models.agent import Agent
 
 class AgentDal:
     db = mysql.connector.connect(
@@ -9,7 +10,6 @@ class AgentDal:
     )
 
     def add_agent(self, code_name: str, real_name: str, location: str, status: str, missions_completed: int):
-
         try:
             with self.db.cursor() as cursor:
                 sql = "INSERT INTO agents (codeName, realName, location, status, missionsCompleted) VALUES (%s, %s, %s, %s, %s)"
@@ -19,6 +19,22 @@ class AgentDal:
                 print("Agent added successfully")
         except mysql.connector.Error as e:
             print(f"Error adding agent: {e}")
+
+    def get_agent(self, code_name: str) -> Agent | None:
+        try:
+            with self.db.cursor() as cursor:
+                sql = "SELECT * FROM agents WHERE  codeName = %s"
+                val = (code_name, )
+                cursor.execute(sql, val)
+                result = cursor.fetchall()[0]
+                agent = Agent(result[0], result[1], result[2], result[3], result[4], result[5])
+                return agent
+        except mysql.connector.Error as e:
+            print(f"Error getting agent: {e}")
+
+
+
+
 
 
 
