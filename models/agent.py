77a29ee865d62e518ea4338b40ddbbed6465ac1b
kodcode <quarts.91.elders@icloud.com>
2025-06-26 15:41:45 +0300

    add __str__

--- models/agent.py
+++ models/agent.py
@@ -3,12 +3,21 @@
 class Agent:
 
     def __init__(self, agent_id: int, code_name: str, real_name: str, location: str, status: str, missions_completed: int):
-        self.agent_id = agent_id
-        self.code_name = code_name
-        self.real_name = real_name
-        self.location = location
-        self.status = status
-        self.missions_completed = missions_completed
+        self.__agent_id = agent_id
+        self.__code_name = code_name
+        self.__real_name = real_name
+        self.__location = location
+        self.__status = status
+        self.__missions_completed = missions_completed
+
+    def __str__(self):
+        info = (f"Agent ID: {self.__agent_id}. \nCode name: {self.__code_name}.\n" +
+                f"Real name: {self.__real_name}. \nLocation: {self.__location}.\n" +
+                f"Status: {self.__status}. \nMissions completed:{self.__missions_completed}"
+                )
+        return info
+
+
 
 
 
