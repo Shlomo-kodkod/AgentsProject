baca52db4ed26b86bbf4ec8dd708fc09586650e0
kodcode <quarts.91.elders@icloud.com>
2025-06-26 13:43:48 +0300

    add creat function

--- dal/agent_dal.py
+++ dal/agent_dal.py
@@ -8,5 +8,17 @@ class AgentDal:
     database="eagleEyeDB"
     )
 
+    def add_agent(self, code_name: str, real_name: str, location: str, status: str, missions_completed: int):
+
+        try:
+            with self.db.cursor() as cursor:
+                sql = "INSERT INTO agents (codeName, realName, location, status, missionsCompleted) VALUES (%s, %s, %s, %s, %s)"
+                val = (code_name, real_name, location, status, missions_completed)
+                cursor.execute(sql, val)
+                self.db.commit()
+                print("Agent added successfully")
+        except mysql.connector.Error as e:
+            print(f"Error adding agent: {e}")
+
 
 
